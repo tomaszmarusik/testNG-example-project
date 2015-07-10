adb0ada55e5cfb28fa4c42f13f1a709f224f96d5
tomasz.marusik <tomasz.marusik@comarch.com>
2015-07-10 16:06:11 +0200

    FirstCommit

---
+++ SimpleCalculator/src/main/java/com/comarch/training/SimpleCalculator/App.java
@@ -0,0 +1,13 @@
+package com.comarch.training.SimpleCalculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
